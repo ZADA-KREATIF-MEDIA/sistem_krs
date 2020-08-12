ae1298303ba336310bdbe8945b8d05ae3c0b6c04
Marcelino15 <tegar.marcelino@gmail.com>
2020-08-12 13:57:43 +0700

    UPDATE 12-08-2020 by marcelino

--- application/models/M_mahasiswa.php
+++ application/models/M_mahasiswa.php
@@ -1,51 +1,50 @@
 <?php
 if(!defined('BASEPATH')) exit('No direct script access allowed');
-Class M_mahasiswa extends CI_Model{
-
-    public function m_get_all_mahasiswa()
+Class M_mahasiswa extends CI_Model
+{
+    public function m_get_matkul_diambil()
     {
-        $this->db->select()
-            ->from('mahasiswa');
+        $this->db->select("a.id, b.id AS id_matkul, b.kode_matkul, b.nama, b.jam_mulai, b.jam_selesai, b.kelas, b.sks, b.tipe")
+            ->from('matakuliah_diambil AS a')
+            ->join('matakuliah AS b', 'b.id = a.id_matakuliah', 'left')
+            ->where("id_mahasiswa", $this->session->userdata('mhs_id'));
         $query = $this->db->get_compiled_select();
+        // print('<pre>');print_r($query);exit;
         $data  = $this->db->query($query)->result_array();
         return $data;
     }
 
-    public function m_simpan_mahasiswa($post)
+    public function m_get_matkul_belum_diambil()
     {
-        $this->db->insert('mahasiswa', $post);
-        return true;
-    }
-
-    public function m_get_mahasiswa_by($id)
-    {
-        $this->db->select()
-            ->from('mahasiswa')
-            ->where("id", $id);
-        $query = $this->db->get_compiled_select();
-        $data  = $this->db->query($query)->row_array();
+        $data_matkul_terambil = $this->m_get_matkul_diambil();
+        // print('<pre>');print_r($data_matkul_terambil);
+        $i=0;
+        foreach($data_matkul_terambil as $val){
+            $this->db->select()
+                ->from('matakuliah')
+                ->where('id !=', $val['id_matkul']);
+            $query = $this->db->get_compiled_select();
+            // print('<pre>');print_r($query);
+            $data  = $this->db->query($query)->result_array();
+            $i++;
+        }
+        // print('<pre>');print_r($data);exit();
         return $data;
     }
 
-    public function m_update_mahasiswa($post)
+    public function m_simpan_ambil_matkul($post)
     {
-        $this->db->select()
-            ->from('mahasiswa')
-            ->where("id", $post['id']);
-        $query = $this->db->set($post)->get_compiled_update();
-        // print('<pre>');print_r($query);exit();
-        $this->db->query($query);
-        return true;	
+        $this->db->insert_batch('matakuliah_diambil', $post);
+		return true;
     }
 
-    public function m_hapus_mahasiswa($id)
+    public function m_hapus_matakuliah_diambil($id)
     {
         $this->db->select()
-            ->from('mahasiswa')
+            ->from('matakuliah_diambil')
             ->where("id", $id);
         $query = $this->db->get_compiled_delete();
         $this->db->query($query);
         return true;
     }
-    
 }
