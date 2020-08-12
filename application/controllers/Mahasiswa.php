9d97efdab5d5ba15e8c53122d4c1a2b80257f93a
Marcelino15 <tegar.marcelino@gmail.com>
2020-08-12 13:57:43 +0700

    UPDATE 12-08-2020 by marcelino

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -9,115 +9,54 @@ class Mahasiswa extends CI_Controller
         
     }
 
-    public function index()
+    public function matakuliah()
     {
-        $data['halaman']    = "Data Mahasiswa";
-        $data['mahasiswa']  = $this->mod->m_get_all_mahasiswa();
+        $data['halaman']    = "Data Matakuliah";
+        $data['matkul_ambil'] = $this->mod->m_get_matkul_diambil();
         // print('<pre>');print_r($data);exit();
         $this->load->view('head.php', $data);
         $this->load->view('sidebar.php');
         $this->load->view('header.php');
-        $this->load->view('mahasiswa/index.php', $data);
+        $this->load->view('mahasiswa/matakuliah.php');
         $this->load->view('footer.php');
     }
 
-    public function tambah_mahasiswa()
+    public function daftar_matakuliah()
     {
-        $data['halaman']    = "TambahData Mahasiswa";
-        $this->load->view('head.php', $data);
-        $this->load->view('sidebar.php');
-        $this->load->view('header.php');
-        $this->load->view('mahasiswa/tambah.php', $data);
-        $this->load->view('footer.php');
-    }
-
-    public function simpan_mahasiswa()
-    {
-        $this->form_validation->set_rules('nama', 'Nama Mahasiswa', 'required');
-        $this->form_validation->set_rules('nim', 'NIM', 'required|numeric');
-        $this->form_validation->set_rules('password', 'Password', 'required');
-        $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|matches[password]');
-        $this->form_validation->set_rules('no_tlpn', 'Nomor Telephone', 'required|numeric');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
-        if ($this->form_validation->run() == FALSE)
-        {
-            $data['halaman']    = "TambahData Mahasiswa";
-            $this->load->view('head.php', $data);
-            $this->load->view('sidebar.php');
-            $this->load->view('header.php');
-            $this->load->view('mahasiswa/tambah.php', $data);
-            $this->load->view('footer.php');
-        }else{
-            $post = [
-                'nim' => $this->input->post('nim', TRUE),
-                'nama' => $this->input->post('nama', TRUE),
-                'password' => password_hash($this->input->post('password', true),PASSWORD_DEFAULT),
-                'jenis_kelamin' => $this->input->post('jenis_kelamin', true),
-                'alamat' => $this->input->post('alamat', true),
-                'tgl_masuk'=> date("Y-m-d"),
-                'nomor_telephone' => $this->input->post('no_tlpn', true),
-                'agama' => $this->input->post('agama', true),
-                'email' => $this->input->post('email', true)
-            ];
-            $this->mod->m_simpan_mahasiswa($post);
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Tambah Data Mahasiswa Berhasil</div>');
-            redirect('mahasiswa');
-        }
-    }
-
-    public function edit_mahasiswa()
-    {
-        $id = $this->uri->segment(3);
-        $data['mahasiswa'] = $this->mod->m_get_mahasiswa_by($id);
+        $data['halaman']    = "Data Matakuliah";
+        $data['matkul']     = $this->mod->m_get_matkul_belum_diambil();
         // print('<pre>');print_r($data);exit();
-        $data['halaman']    = "Edit Data Mahasiswa";
         $this->load->view('head.php', $data);
         $this->load->view('sidebar.php');
         $this->load->view('header.php');
-        $this->load->view('mahasiswa/edit.php', $data);
+        $this->load->view('mahasiswa/daftar_matakuliah.php',$data);
         $this->load->view('footer.php');
     }
 
-    public function update_mahasiswa()
+    public function ambil_matkul()
     {
-        $this->form_validation->set_rules('nama', 'Nama Mahasiswa', 'required');
-        $this->form_validation->set_rules('nim', 'NIM', 'required|numeric');
-        $this->form_validation->set_rules('no_tlpn', 'Nomor Telephone', 'required|numeric');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
-        if ($this->form_validation->run() == FALSE)
-        {
-            $data['halaman']    = "Edit Data Mahasiswa";
-            $this->load->view('head.php', $data);
-            $this->load->view('sidebar.php');
-            $this->load->view('header.php');
-            $this->load->view('mahasiswa/edit.php', $data);
-            $this->load->view('footer.php');
-        }else{
-            $post = [
-                'id'    => $this->input->post('id', true),
-                'nim' => $this->input->post('nim', TRUE),
-                'nama' => $this->input->post('nama', TRUE),
-                'jenis_kelamin' => $this->input->post('jenis_kelamin', true),
-                'alamat' => $this->input->post('alamat', true),
-                'tgl_masuk'=> date("Y-m-d"),
-                'nomor_telephone' => $this->input->post('no_tlpn', true),
-                'agama' => $this->input->post('agama', true),
-                'email' => $this->input->post('email', true)
+        $matkul_pilih = $this->input->post('matkul_pilih', true);
+        $i=0;
+        foreach($matkul_pilih as $mp){
+            $post[$i] = [
+                'id_matakuliah' => $mp,
+                'id_mahasiswa'  => $this->session->userdata('mhs_id')
             ];
-            // print('<pre>');print_r($post);exit();
-            $this->mod->m_update_mahasiswa($post);
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Update Data Mahasiswa Berhasil</div>');
-            redirect('mahasiswa');
+            $i++;
         }
+        // print('<pre>');print_r($post);exit();
+        $this->mod->m_simpan_ambil_matkul($post);
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Matakuliah Berhasil Di tambah</div>');
+        redirect('mahasiswa/matakuliah');
     }
 
-    public function hapus_mahasiswa()
+    public function hapus_matakuliah_diambil()
     {
         $id = $this->uri->segment(3);
-        $this->mod->m_hapus_mahasiswa($id);
-        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data Mahasiswa Berhasil Dihapus</div>');
-        redirect('mahasiswa');
+        $this->mod->m_hapus_matakuliah_diambil($id);
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Matakuliah Berhasil Dihapus</div>');
+        redirect('mahasiswa/matakuliah');
     }
+
+    
 }
---
+++ application/models/M_admin.php
@@ -0,0 +1,178 @@
+<?php
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+Class M_admin extends CI_Model
+{
+    public function m_get_all_mahasiswa()
+    {
+        $this->db->select()
+            ->from('mahasiswa');
+        $query = $this->db->get_compiled_select();
+        $data  = $this->db->query($query)->result_array();
+        return $data;
+    }
+
+    public function m_simpan_mahasiswa($post)
+    {
+        $this->db->insert('mahasiswa', $post);
+        return true;
+    }
+
+    public function m_get_mahasiswa_by($id)
+    {
+        $this->db->select()
+            ->from('mahasiswa')
+            ->where("id", $id);
+        $query = $this->db->get_compiled_select();
+        $data  = $this->db->query($query)->row_array();
+        return $data;
+    }
+
+    public function m_update_mahasiswa($post)
+    {
+        $this->db->select()
+            ->from('mahasiswa')
+            ->where("id", $post['id']);
+        $query = $this->db->set($post)->get_compiled_update();
+        // print('<pre>');print_r($query);exit();
+        $this->db->query($query);
+        return true;	
+    }
+
+    public function m_hapus_mahasiswa($id)
+    {
+        $this->db->select()
+            ->from('mahasiswa')
+            ->where("id", $id);
+        $query = $this->db->get_compiled_delete();
+        $this->db->query($query);
+        return true;
+    }
+
+    public function m_get_all_dosen()
+    {
+        $this->db->select()
+            ->from('dosen');
+        $query = $this->db->get_compiled_select();
+        $data  = $this->db->query($query)->result_array();
+        return $data;
+    }
+
+    public function m_simpan_dosen($post)
+    {
+        $this->db->insert('dosen', $post);
+        return true;
+    }
+
+    public function m_get_dosen_by($id)
+    {
+        $this->db->select()
+            ->from('dosen')
+            ->where("id", $id);
+        $query = $this->db->get_compiled_select();
+        $data  = $this->db->query($query)->row_array();
+        return $data;
+    }
+
+    public function m_update_dosen($post)
+    {
+        $this->db->select()
+            ->from('dosen')
+            ->where("id", $post['id']);
+        $query = $this->db->set($post)->get_compiled_update();
+        // print('<pre>');print_r($query);exit();
+        $this->db->query($query);
+        return true;	
+    }
+
+    public function m_hapus_dosen($id)
+    {
+        $this->db->select()
+            ->from('dosen')
+            ->where("id", $id);
+        $query = $this->db->get_compiled_delete();
+        $this->db->query($query);
+        return true;
+    }
+
+    public function m_get_all_akademik()
+    {
+        $this->db->select()
+            ->from('akademik');
+        $query = $this->db->get_compiled_select();
+        $data  = $this->db->query($query)->result_array();
+        return $data;
+    }
+
+    public function m_simpan_akademik($post)
+    {
+        $this->db->insert('akademik', $post);
+        return true;
+    }
+
+    public function m_update_akademik($post)
+    {
+        $this->db->select()
+            ->from('akademik')
+            ->where("id", $post['id']);
+        $query = $this->db->set($post)->get_compiled_update();
+        // print('<pre>');print_r($query);exit();
+        $this->db->query($query);
+        return true;	
+    }
+
+    public function m_hapus_akademik($id)
+    {
+        $this->db->select()
+            ->from('akademik')
+            ->where("id", $id);
+        $query = $this->db->get_compiled_delete();
+        $this->db->query($query);
+        return true;
+    }
+
+    public function m_get_all_matakuliah()
+    {
+        $this->db->select()
+            ->from('matakuliah');
+        $query = $this->db->get_compiled_select();
+        $data  = $this->db->query($query)->result_array();
+        return $data;
+    }
+
+    public function m_simpan_matakuliah($post)
+    {
+        $this->db->insert('matakuliah', $post);
+        return true;
+    }
+
+    public function m_get_matakuliah_by($id)
+    {
+        $this->db->select()
+            ->from('matakuliah')
+            ->where("id", $id);
+        $query = $this->db->get_compiled_select();
+        $data  = $this->db->query($query)->row_array();
+        return $data;
+    }
+
+    public function m_update_matakuliah($post)
+    {
+        $this->db->select()
+            ->from('matakuliah')
+            ->where("id", $post['id']);
+        $query = $this->db->set($post)->get_compiled_update();
+        // print('<pre>');print_r($query);exit();
+        $this->db->query($query);
+        return true;	
+    }
+
+    public function m_hapus_matakuliah($id)
+    {
+        $this->db->select()
+            ->from('matakuliah')
+            ->where("id", $id);
+        $query = $this->db->get_compiled_delete();
+        $this->db->query($query);
+        return true;
+    }
+}
